d39621c9d00b6a1877d01f0372eea4228494c7e3
Qeuroal <Qeuroal@163.com>
2024-05-22 09:16:38 +0800

    wezterm

---
+++ assets/wezterm/.wezterm.lua
@@ -0,0 +1,9 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+config.color_scheme = 'OneDark (base16)'
+
+config.font = wezterm.font('JetBrains Mono', { weight = 'Regular', italic = false })
+config.font_size = 15.0
+
+return  config
